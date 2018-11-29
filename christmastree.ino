6ed9882996553a052570e018cc641923d9cb205e
Nida Ahmed <nahmed@cshnyc.org>
2018-11-29 14:52:32 -0500

    29/11/2018

--- christmastree.ino
+++ christmastree.ino
@@ -1,12 +1,18 @@
 
-int LED = 3;
+int LED = 4;
+int BlueLED = 3;
 void setup() {
   pinMode(LED, OUTPUT);
+  pinMode(BlueLED, OUTPUT);
 }
 
 void loop() {
   digitalWrite(LED, HIGH);
   delay(1000);
   digitalWrite(LED, LOW);
+  delay(1000);
+  digitalWrite(BlueLED, HIGH);
+  delay(1000);
+  digitalWrite(BlueLED, LOW);
   delay(1000);
 }
